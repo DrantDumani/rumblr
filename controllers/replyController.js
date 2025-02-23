6198eb64c786bde2d7d43c4a611013ae9fa95859
Darnell <darnjames@gmail.com>
2025-02-23 13:23:29 -0500

    Fix bug where replies were not being related with parent post and include author data

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -3,15 +3,35 @@ const { Prisma } = require('@prisma/client');
 
 exports.createReply = async (req, res, next) => {
   try {
+    // fetch the post first to see if it has a parent id you can use
+    const postToReply = await client.post.findUnique({
+      where: {
+        id: Number(req.params.postId),
+        isDeleted: false,
+      },
+      select: {
+        id: true,
+        parent_id: true,
+      },
+    });
+
     const reply = await client.reply.create({
       data: {
         author_id: req.user.id,
         content: req.body.content,
-        post_id: Number(req.params.postId),
+        post_id: postToReply.parent_id || postToReply.id,
+      },
+      include: {
+        author: {
+          select: {
+            uname: true,
+            pfp: true,
+          },
+        },
       },
     });
 
-    return res.json({ reply_id: reply.id });
+    return res.json({ reply_id: reply.id, reply });
   } catch (e) {
     if (e instanceof Prisma.PrismaClientValidationError)
       return res.status(400).json('Bad Request');
@@ -38,15 +58,27 @@ exports.deleteReply = async (req, res, next) => {
 exports.getReplies = async (req, res, next) => {
   try {
     const filterOpts = {
+      where: {
+        post_id: Number(req.params.postId),
+      },
       take: 10,
       orderBy: {
         id: 'desc',
+      },
+      include: {
+        author: {
+          select: {
+            uname: true,
+            pfp: true,
+          },
+        },
       },
     };
     if (req.body.cursor) {
       filterOpts.cursor = req.body.cursor;
     }
     const replies = await client.reply.findMany(filterOpts);
+
     return res.json(replies);
   } catch (e) {
     return next(e);
